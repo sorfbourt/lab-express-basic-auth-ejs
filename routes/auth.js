153fe27df36a84f3cdc4beb47c576a270f5d5135
Krystina Witt <krystinawitt@gmail.com>
2023-02-07 19:51:52 +0100

    iteration 1 done

--- routes/auth.js
+++ routes/auth.js
@@ -17,8 +17,15 @@ router.post("/signup", async (req, res, next) => {
   const body = { ...req.body };
   console.log(body)
 
+
+  if (body.password.length < 6) {
+    res.render('auth/signup', { errorMessage: 'Password too short', body: req.body, userData: req.body})
+  } else {
+
+
   const salt = bcrypt.genSaltSync(10);
   const passwordHash = bcrypt.hashSync(body.password, salt);
+  console.log(passwordHash)
 
   delete body.password;
   body.passwordHash = passwordHash
@@ -27,12 +34,25 @@ router.post("/signup", async (req, res, next) => {
     await UserModel.create(body);
     res.redirect("/auth/signup");
   } catch (error) {
-    console.log(error);
+    console.log("error in post signup", error)
+    if(error.code == 11000){
+      console.log("Duplicate!")
+      res.render("auth/signup", {errorMessage: "Username already used!", userData: req.body})
+    }
+    else{
+      res.render("auth/signup", {errorMessage: error, userData: req.body})
+    }
   }
 
-
+  }
 
 });
+
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
 
 
 
